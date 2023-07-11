6438529f192ce961f2012dcd3aac985ec618fb55
Guilherme Silva de Castro <guiccastro@outlook.com>
2023-07-10 23:45:21 -0300

    Ajuste nas cores do tipo

--- app/src/main/java/com/project/mypokedex/ui/components/PokemonTypeUI.kt
+++ app/src/main/java/com/project/mypokedex/ui/components/PokemonTypeUI.kt
@@ -1,19 +1,18 @@
 package com.project.mypokedex.ui.components
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.project.mypokedex.model.PokemonType
+import com.project.mypokedex.ui.theme.BorderBlack
 import com.project.mypokedex.ui.theme.PokemonGB
 
 @Composable
@@ -23,18 +22,15 @@ fun PokemonTypeToUI(
     fontSize: TextUnit = 6.sp
 ) {
     val cornerRoundWeight = 3 / 4
-    val cornerShape = RoundedCornerShape((fontSize.value * cornerRoundWeight).dp)
     Text(
         modifier = modifier
-            .border(width = (fontSize.value / 35).dp, Color.Black, cornerShape)
-            .border(width = (fontSize.value / 7).dp, color = pokemonType.getColor(), cornerShape)
-            .background(Color.Black.copy(alpha = 0.5f), cornerShape)
+            .background(pokemonType.getColor(), CircleShape)
             .padding(
                 horizontal = (fontSize.value * 2 / 3).dp,
                 vertical = (fontSize.value / 2).dp
             ),
         text = pokemonType.toString().uppercase(),
-        color = pokemonType.getColor(),
+        color = BorderBlack,
         fontSize = fontSize,
         fontWeight = FontWeight(1000),
         style = PokemonGB
--- app/src/main/java/com/project/mypokedex/ui/theme/Theme.kt
+++ app/src/main/java/com/project/mypokedex/ui/theme/Theme.kt
@@ -1,9 +1,12 @@
 package com.project.mypokedex.ui.theme
 
+import androidx.compose.foundation.LocalIndication
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material.ripple.rememberRipple
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.platform.LocalView
 
 private val LightColorScheme = lightColorScheme(
@@ -38,7 +41,11 @@ fun MyPokedexTheme(
 
     MaterialTheme(
         colorScheme = colorScheme,
-        typography = Typography,
-        content = content
-    )
+        typography = Typography
+    ) {
+        CompositionLocalProvider(
+            LocalIndication provides rememberRipple(color = BorderBlack),
+            content = content,
+        )
+    }
 }
