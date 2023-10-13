9ceb1802df3fa4deb2d129aad96bc3fa23a07387
Guilherme Silva de Castro <guiccastro@outlook.com>
2023-10-13 16:32:55 -0300

    Implementação de um inputText melhor e mais genérico

--- app/src/main/java/com/project/mypokedex/ui/components/InputTexts.kt
+++ app/src/main/java/com/project/mypokedex/ui/components/InputTexts.kt
@@ -1,24 +1,38 @@
 package com.project.mypokedex.ui.components
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.text.selection.TextSelectionColors
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.project.mypokedex.R
 import com.project.mypokedex.ui.theme.BlackTextColor
 import com.project.mypokedex.ui.theme.MainBlack
+import com.project.mypokedex.ui.theme.MainBlue
 import com.project.mypokedex.ui.theme.MainSelectionTextBackground
+import com.project.mypokedex.ui.theme.MyPokedexTheme
 import com.project.mypokedex.ui.theme.PokemonGB
 import com.project.mypokedex.ui.theme.SearchTextBackground
 
@@ -72,4 +86,71 @@ fun SearchInputText(
             keyboardType = KeyboardType.Password
         )
     )
+}
+
+@Composable
+fun CustomTextField(
+    text: String,
+    onValueChange: (String) -> Unit,
+    modifier: Modifier = Modifier,
+    placeholderText: String = "Placeholder",
+    textStyle: TextStyle = PokemonGB,
+    textSize: TextUnit = 10.sp,
+    textColor: Color = MainBlack,
+    leadingIcon: (@Composable () -> Unit)? = null,
+    trailingIcon: (@Composable () -> Unit)? = null,
+    keyboardOptions: KeyboardOptions = KeyboardOptions.Default,
+    shape: Shape = RoundedCornerShape(4.dp),
+    borderWidth: Dp = 1.dp,
+    borderColor: Color = MainBlack,
+    backgroundColor: Color = MainBlue
+
+) {
+    BasicTextField(
+        value = text,
+        onValueChange = {
+            onValueChange(it)
+        },
+        textStyle = textStyle.copy(fontSize = textSize, color = textColor),
+        singleLine = true,
+        modifier = modifier
+            .border(borderWidth, borderColor, shape)
+            .background(backgroundColor, shape),
+        keyboardOptions = keyboardOptions,
+        decorationBox = { innerTextField ->
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 6.dp, horizontal = 8.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                if (leadingIcon != null) leadingIcon()
+                Box(Modifier.weight(1f)) {
+                    if (text.isEmpty()) Text(
+                        placeholderText,
+                        style = textStyle.copy(
+                            fontSize = textSize,
+                            color = textColor.copy(alpha = 0.7F)
+                        )
+                    )
+                    innerTextField()
+                }
+                if (trailingIcon != null) trailingIcon()
+            }
+        }
+    )
+}
+
+@Preview
+@Composable
+fun InputHeightDialogPreview() {
+    MyPokedexTheme {
+        Surface {
+            CustomTextField(
+                text = "",
+                onValueChange = {},
+                placeholderText = "Placeholder"
+            )
+        }
+    }
 }
--- app/src/main/java/com/project/mypokedex/ui/screens/DetailsUIScreen.kt
+++ app/src/main/java/com/project/mypokedex/ui/screens/DetailsUIScreen.kt
@@ -15,14 +15,20 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Alignment.Companion.Center
 import androidx.compose.ui.Alignment.Companion.CenterHorizontally
@@ -35,11 +41,13 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
 import coil.compose.SubcomposeAsyncImage
 import coil.imageLoader
 import com.project.mypokedex.R
@@ -54,6 +62,7 @@ import com.project.mypokedex.sampledata.charizard
 import com.project.mypokedex.sampledata.charmander
 import com.project.mypokedex.sampledata.squirtle
 import com.project.mypokedex.ui.components.CardScreen
+import com.project.mypokedex.ui.components.CustomTextField
 import com.project.mypokedex.ui.components.PokemonGenerationToUI
 import com.project.mypokedex.ui.components.PokemonImage
 import com.project.mypokedex.ui.components.PokemonTypeToUI
@@ -109,6 +118,58 @@ fun DetailsUIScreen(state: DetailsScreenUIState) {
 
                 AdvancedDetails(pokemon = pokemon, personHeight = 1.84F * 100)
             }
+        }
+    }
+}
+
+@Composable
+fun InputHeightDialog() {
+    Dialog(onDismissRequest = { }) {
+        Column(
+            modifier = Modifier
+                .widthIn(max = 250.dp)
+                .fillMaxWidth()
+                .shadow(4.dp, RoundedCornerShape(4.dp))
+                .background(MainWhite, RoundedCornerShape(10.dp))
+                .border(2.dp, MainBlack, RoundedCornerShape(10.dp)),
+            horizontalAlignment = CenterHorizontally
+        ) {
+            Text(
+                text = "Input your height.",
+                color = BlackTextColor,
+                fontSize = 12.sp,
+                style = PokemonGB,
+                textAlign = TextAlign.Center,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 10.dp, vertical = 8.dp)
+            )
+
+            Text(
+                text = "The value must be in centimeter.",
+                color = BlackTextColor,
+                fontSize = 8.sp,
+                style = PokemonGB,
+                textAlign = TextAlign.Center,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 10.dp)
+                    .padding(bottom = 8.dp)
+            )
+
+            var text by rememberSaveable { mutableStateOf("") }
+            CustomTextField(
+                text = text,
+                onValueChange = {
+                    text = it
+                },
+                placeholderText = "Height",
+                modifier = Modifier
+                    .padding(horizontal = 10.dp)
+                    .padding(bottom = 10.dp)
+                    .widthIn(max = 100.dp),
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Decimal)
+            )
         }
     }
 }
