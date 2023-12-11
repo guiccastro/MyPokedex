a4a83f40b151b353b940b6d50e481a9602d08dff
Guilherme Silva de Castro <guiccastro@outlook.com>
2023-12-11 15:06:53 -0300

    Mostrar linguagem nas configurações

--- app/src/main/java/com/project/mypokedex/ui/screens/SettingsUIScreen.kt
+++ app/src/main/java/com/project/mypokedex/ui/screens/SettingsUIScreen.kt
@@ -4,17 +4,21 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.project.mypokedex.LanguageCentral
 import com.project.mypokedex.extensions.bottomBorder
 import com.project.mypokedex.extensions.topBorder
 import com.project.mypokedex.model.SettingsOptionItem
@@ -34,11 +38,8 @@ fun SettingsUIScreen() {
         verticalArrangement = Arrangement.spacedBy(10.dp)
     ) {
         SettingsOptionItem.values().forEach { option ->
-            Text(
-                text = stringResource(id = option.title),
-                style = PokemonGB,
-                color = BlackTextColor,
-                fontSize = 18.sp,
+
+            Row(
                 modifier = Modifier
                     .fillMaxWidth()
                     .topBorder(1.dp, MainBlack)
@@ -56,8 +57,30 @@ fun SettingsUIScreen() {
                         }
                     }
                     .background(MainRed.copy(alpha = 1F))
-                    .padding(vertical = 10.dp, horizontal = 10.dp)
-            )
+                    .padding(vertical = 10.dp, horizontal = 10.dp),
+                verticalAlignment = CenterVertically
+            ) {
+                Text(
+                    text = stringResource(id = option.title),
+                    style = PokemonGB,
+                    color = BlackTextColor,
+                    fontSize = 18.sp,
+                    modifier = Modifier
+                        .weight(1F)
+                )
+
+                if (option == SettingsOptionItem.LanguageSettingItem) {
+                    Text(
+                        text = LanguageCentral.getCurrentLanguage().toLanguageTag().uppercase(),
+                        style = PokemonGB,
+                        color = BlackTextColor.copy(alpha = 0.5F),
+                        fontSize = 12.sp,
+                        fontWeight = FontWeight.Bold,
+                        modifier = Modifier
+                            .padding(horizontal = 10.dp)
+                    )
+                }
+            }
         }
     }
 }
